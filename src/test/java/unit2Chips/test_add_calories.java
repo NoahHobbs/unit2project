2711720fb4c979b568868234d76ab09e5241bb73
Noah Hobbs <mlzgphoenix@gmail.com>
2021-02-04 14:13:40 -0600

    added another test for addCalories method

--- src/test/java/unit2Chips/test_add_calories.java
+++ src/test/java/unit2Chips/test_add_calories.java
@@ -15,10 +15,21 @@ public class test_add_calories {
 
 	@Test
 	public void test_add_calories_succeed() {
+		// this test is for the addCalories class to ensure that the calories are being added if the starting calorie is low enough
 		chips.setCalories(200);
 		chM.addCalories(chips);
 		int totalCal = chips.getCalories();
 		assertEquals(totalCal, 450);
 	}
+	
+	@Test
+	public void test_add_calories_not_adding() {
+		//just making sure that the calories don't just add no matter what
+		chips.setCalories(600);
+		chM.addCalories(chips);
+		int totalCal = chips.getCalories();
+		assertEquals(totalCal, 600);
+	}
+	
 
 }
